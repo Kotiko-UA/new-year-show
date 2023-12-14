47ef5f87f578e3a98e51fa267d565f8dde4a42de
Kotiko <124360315+Kotiko-UA@users.noreply.github.com>
2023-12-14 17:48:19 +0200

    add paralax

--- js/main.js
+++ js/main.js
@@ -13,3 +13,47 @@ function onSubmit(e) {
     compliteText.style.display = 'none';
   }, 5000);
 }
+
+const mainBackground = document.querySelector('#main-bgd');
+
+const startBgdParallaxAnim = (e) => {
+  const bgd = document.querySelector('#main-bgd');
+  const speed = bgd.getAttribute('data-speed');
+  const { clientX: mouseX, clientY: mouseY } = e;
+
+  const x = (window.innerHeight - e.pageX * speed) / 100;
+  const y = (window.innerWidth - e.pageY * speed) / 100;
+
+  bgd.style.transform = `translateX(${x}px) translateY(${y}px)`;
+};
+
+const startProjectorParallaxAnim = (e) => {
+  const projectors = document.querySelectorAll('.projector');
+
+  projectors.forEach((projector) => {
+    const speed = projector.getAttribute('data-speed');
+    const { clientX: mouseX, clientY: mouseY } = e;
+
+    const x = (window.innerHeight - e.pageX * speed) / 100;
+    const y = (window.innerWidth - e.pageY * speed) / 100;
+
+    projector.style.transform = `translateX(${x}px) translateY(${y}px)`;
+  });
+};
+
+const startLogoAnim = (e) => {
+  const logo = document.querySelector('#main-toy-img');
+  const speed = logo.getAttribute('data-speed');
+  const { clientX: mouseX, clientY: mouseY } = e;
+
+  const x = (window.innerHeight - e.pageX * speed) / 100;
+  const y = (window.innerWidth - e.pageY * speed) / 100;
+
+  logo.style.transform = `translateX(${x}px) translateY(${y}px)`;
+};
+
+document.addEventListener('mousemove', (e) => {
+  startBgdParallaxAnim(e);
+  startProjectorParallaxAnim(e);
+  startLogoAnim(e);
+});
